5f392d57c33bd480ee560c04505cb539938141c2
taohansens <taohansens@gmail.com>
2023-03-12 21:30:30 -0300

    Create Project. First commit.

---
+++ src/main/java/br/edu/infnet/rentgo/RentGoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.rentgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/infnet/rentgo/RentGoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.rentgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
