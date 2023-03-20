b19dab33c9a2f91db8a008955eec05de1fbd1e7c
taohansens <taohansens@gmail.com>
2023-03-19 23:43:39 -0300

    Feature 5 [FINISHED] Dataloaders ok.

--- src/main/java/br/edu/infnet/rentgo/StoreLoader.java
+++ src/main/java/br/edu/infnet/rentgo/StoreLoader.java
@@ -1,10 +1,7 @@
 package br.edu.infnet.rentgo;
 
-import br.edu.infnet.rentgo.dtos.ClientDTO;
-import br.edu.infnet.rentgo.dtos.CredentialUserDTO;
-import br.edu.infnet.rentgo.entities.Client;
-import br.edu.infnet.rentgo.entities.CredentialUser;
-import br.edu.infnet.rentgo.services.ClientService;
+import br.edu.infnet.rentgo.dtos.StoreDTO;
+import br.edu.infnet.rentgo.services.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,22 +11,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Component
-public class ClientLoader implements ApplicationRunner {
+public class StoreLoader implements ApplicationRunner {
 
     @Autowired
-    private ClientService clientService;
+    private StoreService storeService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(LocalDateTime.now() + " [ClientLoader] Init.");
+        System.out.println(LocalDateTime.now() + " [Store Loader] Init.");
         try {
-            String arquivo = "DATAclients.txt";
-            try {
+            String arquivo = "DATAstore.txt";
+            try{
                 FileReader fileR = new FileReader(arquivo);
                 BufferedReader leitura = new BufferedReader(fileR);
 
@@ -38,17 +32,17 @@ public class ClientLoader implements ApplicationRunner {
 
                 while (linha != null) {
                     campos = linha.split(";");
-                    ClientDTO clientDTO = new ClientDTO(campos[0], campos[1], campos[2], campos[3], campos[4], campos[5], campos[6], campos[7], campos[8], campos[9], campos[10], campos[11], Boolean.parseBoolean(campos[12]));
-                    clientService.insert(clientDTO);
+                    StoreDTO storeDTO = new StoreDTO(campos[0],campos[1],campos[2],campos[3],campos[4],campos[5],campos[6]);
+                    storeService.insert(storeDTO);
                     linha = leitura.readLine();
                 }
                 leitura.close();
                 fileR.close();
-            } catch (IOException e) {
+            } catch (IOException e){
                 System.out.println("[ERRO] " + e.getMessage());
             }
         } finally {
-            System.out.println(LocalDateTime.now() + " [ClientLoader] Finished. Users Added.");
+            System.out.println(LocalDateTime.now() + " [Store Loader] Finished. Users Added.");
         }
     }
 }
